62b7b8dd94d465ce5e57dce0b7f518a9f8452176
xukai <xukai19871105@126.com>
2019-05-21 22:21:29 +0800

    取消utlist链表使用，全部使用fdset类型

--- tcp-demo/tcp-select-server.c
+++ tcp-demo/tcp-select-server.c
@@ -10,28 +10,29 @@
 #include <arpa/inet.h>
 #include <sys/select.h>
 #include <assert.h>
-#include "utlist.h"
-
 
 #define BUF_SIZE    1024
 
-
-typedef struct fd_el
+void print_sockaddr(struct sockaddr_in addr)
 {
-    int fd;
-    struct fd_el *next, *prev;
-} fd_el_t;
+    // 保存点分十进制的地址
+    char ip_address[INET_ADDRSTRLEN];
+    int port;
 
+    inet_ntop(AF_INET, &addr.sin_addr, ip_address, sizeof(ip_address));
+    port = ntohs(addr.sin_port);
+    printf("(%s:%d)\n", ip_address, port);
+}
 
 int main(int argc, char *argv[])
 {
     int sfd = -1;
-    fd_el_t *e = NULL;
-    fd_el_t *head = NULL;
-    fd_el_t *tmp = NULL;
 
-    struct sockaddr_in ser_addr;
+    struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
+    struct sockaddr_in peer_addr;
+    int peer_addrlen;
+    int client_addrlen;
     int optval, maxsockfd;
 
     char recv_buf[BUF_SIZE];
@@ -47,16 +48,16 @@ int main(int argc, char *argv[])
         port = 5678;
     }
 
-    printf("listen port %d, fd = %d  \n", port, sfd);
+    printf("listen port %d, server_fd: %d  \n", port, sfd);
     setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof (optval));
 
     memset(&client_addr, 0, sizeof(client_addr));
-    memset(&ser_addr, 0, sizeof (ser_addr));
-    ser_addr.sin_family = AF_INET;
-    ser_addr.sin_port = htons(port);
-    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(&server_addr, 0, sizeof (server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind (sfd, (struct sockaddr *) &ser_addr, sizeof (ser_addr) < 0)) {
+    if (bind (sfd, (struct sockaddr *) &server_addr, sizeof (server_addr)) < 0) {
         printf("bind failed\n");
     }
 
@@ -64,82 +65,71 @@ int main(int argc, char *argv[])
         printf("listen failed");
     }
 
-    fd_set fdset;
+    fd_set readfds;
+    fd_set workfds;
     maxsockfd = sfd;
     struct timeval tout;
-    
-    while (1)
-    {
-        FD_ZERO(&fdset);
-        FD_SET(sfd, &fdset);
 
+    FD_ZERO(&readfds);
+    FD_SET(sfd, &readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    while (1) {
         struct timeval tout;
-        tout.tv_sec = 5;
+        tout.tv_sec = 2;
         tout.tv_usec = 0;
 
-        LL_FOREACH(head, e) {
-            if (e->fd != 0) {
-                printf("%02d ", e->fd);
-                FD_SET(e->fd, &fdset);
-            }
-        }
-        printf("\n");
-
-        int ret = select (maxsockfd + 1, &fdset, NULL, NULL, &tout);
+        workfds = readfds;
+        int ret = select (maxsockfd + 1, &workfds, NULL, NULL, &tout);
 
         if (ret == 0) {
             continue;
         }
         if (ret == -1) {
-            printf("Error select ...!\n");
             continue;
         }
 
-        if (FD_ISSET(sfd, &fdset))
-        {
-            int cfd_len = sizeof (client_addr);
-            int cfd = accept(sfd, (struct sockaddr *)&client_addr,
-                             (socklen_t *)&cfd_len);
-            printf("accept fd %d \n", cfd);
-
-            if (cfd < 0) {
-                printf("accept cfd <= 0!");
+        for (int i = 0; i < maxsockfd + 1; i++) {
+            if (!FD_ISSET(i, &workfds)) {
                 continue;
             }
 
-            fd_el_t *new_el = (fd_el_t*)malloc(sizeof(fd_el_t));
-            new_el->fd = cfd;
-            LL_PREPEND(head, new_el);
+            int fd = i;
+            if (fd == sfd) {
+                client_addrlen = sizeof(client_addr);
+                int cfd = accept(sfd, (struct sockaddr *)&client_addr,
+                                 (socklen_t *)&client_addrlen);
+                printf("accept fd %d ", cfd);
+                print_sockaddr(client_addr);
 
-            if (cfd > maxsockfd) {
-                maxsockfd = cfd;
-            }
-        }
-
-        LL_FOREACH_SAFE(head, e, tmp) {
-            if (FD_ISSET(e->fd, &fdset)) {
-                ssize_t len = recv(e->fd, recv_buf, sizeof (recv_buf), 0);
-                if (len <= 0) {
-                    printf ("client fd %d has left\n", e->fd);
-                    close(e->fd);
-                    FD_CLR(e->fd, &fdset);
-                    LL_DELETE(head, e);
-                    free(e);
+                if (cfd < 0) {
+                    printf("accept cfd < 0!");
                     continue;
                 }
-
-                printf("the client_ip : %s\n", inet_ntoa(client_addr.sin_addr));
+                FD_SET(cfd, &readfds);
+                if (cfd > maxsockfd) {
+                    maxsockfd = cfd;
+                }
+            } else {
+                ssize_t len = recv(fd, recv_buf, sizeof(recv_buf), 0);
+                if (len <= 0) {
+                    printf ("client fd %d has left\n", fd);
+                    close(fd);
+                    FD_CLR(fd, &readfds);
+                    continue;
+                }
+                // printf("the client_ip : %s\n", inet_ntoa(client_addr.sin_addr));
                 recv_buf[len] = '\0';
-                printf("%s\n", recv_buf);
-                send(e->fd, recv_buf, (size_t)len, 0);
+                printf("%s ", recv_buf);
+                peer_addrlen = sizeof(peer_addr);
+                getpeername(fd, (struct sockaddr *)&peer_addr, (socklen_t *)&peer_addrlen);
+                print_sockaddr(peer_addr);
+
+                send(fd, recv_buf, (size_t)len, 0);
             }
         }
-
     }
 
-    LL_FOREACH(head, e) {
-        close(e->fd);
-    }
 
     return EXIT_SUCCESS;
 }
